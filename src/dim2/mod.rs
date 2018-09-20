4090385df809fa9cc3d21bc40a9d956dfbf779a4
Martin Sandgren <carlmartus@gmail.com>
2018-09-20 23:23:46 +0200

    Extenden 2D Point with operations
    Similar to 3D Vector.

--- src/dim2/mod.rs
+++ src/dim2/mod.rs
@@ -1,11 +1,9 @@
 mod csg;
 mod line_strip;
+mod point;
 mod shapes;
 
 pub use self::csg::Csg;
 pub use self::line_strip::LineStrip;
+pub use self::point::Point;
 pub use self::shapes::{circle, rectangle};
-use Unit;
-
-#[derive(Clone, Copy)]
-pub struct Point(pub Unit, pub Unit);
---
+++ src/dim2/point.rs
@@ -0,0 +1,59 @@
+use std::ops::{Add, Div, Mul, Neg, Sub};
+use Unit;
+
+#[derive(Clone, Copy)]
+pub struct Point(pub Unit, pub Unit);
+
+impl Point {
+    pub fn negate(&self) -> Point {
+        Point(-self.0, -self.1)
+    }
+
+    pub fn dot(&self, other: Point) -> Unit {
+        self.0 * other.0 + self.1 * other.1
+    }
+
+    pub fn length(&self) -> Unit {
+        self.dot(*self).sqrt()
+    }
+
+    pub fn orthogonal(&self) -> Point {
+        Point(-self.1, self.0)
+    }
+}
+
+impl Add for Point {
+    type Output = Point;
+    fn add(self, other: Point) -> Point {
+        Point(self.0 + other.0, self.1 + other.1)
+    }
+}
+
+impl Sub for Point {
+    type Output = Point;
+    fn sub(self, other: Point) -> Point {
+        Point(self.0 - other.0, self.1 - other.1)
+    }
+}
+
+impl Neg for Point {
+    type Output = Point;
+    fn neg(self) -> Point {
+        self.negate()
+    }
+}
+
+impl Mul<Unit> for Point {
+    type Output = Point;
+    fn mul(self, rhs: Unit) -> Point {
+        Point(self.0 * rhs, self.1 * rhs)
+    }
+}
+
+impl Div<Unit> for Point {
+    type Output = Point;
+    fn div(self, rhs: Unit) -> Point {
+        let inv = (1 as Unit) / rhs;
+        Point(self.0 * inv, self.1 * inv)
+    }
+}
